c0c260e64e1ac3fbc85e36bd218caa71bebd6fb6
Ulyses Rico <ulyses@quart.com.mx>
2020-08-24 16:36:29 -0500

    ...

--- test.py
+++ test.py
@@ -1,51 +1,45 @@
-from cassandra.cluster import Cluster
-from cassandra.auth import PlainTextAuthProvider
-from cassandra.query import SimpleStatement
 import os
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
-
 pathToHere=os.getcwd()
+
+
 
 def main():
 
+    querySt="select * from test.tbthesis where period_number>4 ALLOW FILTERING "
 
     #Datastax configuration
-    objCC=CassandraConnection()
-    cloud_config= {
-            'secure_connect_bundle': pathToHere+'\\secure-connect-dbtest.zip'
-    }
     
-    auth_provider = PlainTextAuthProvider(objCC.cc_user_test,objCC.cc_pwd_test)
+    # Creating PySpark Context
+    sc = SparkContext(conf=SparkConf().setAppName("appTest").setMaster("local"))
+    sqlContext = SQLContext(sc)
 
-    cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
-    session = cluster.connect()
-    session.default_timeout=70
+    table_df = sqlContext.read.format("org.apache.spark.sql.cassandra").options(table='tbthesis', keyspace='test').load()
+
+    print("OK...")    
 
     #Spark configuration
-    querySt="select * from test.tbthesis where period_number>4 ALLOW FILTERING "
     
-    conf = SparkConf().setAppName("spark_cassandra_test").setMaster("local")
-
-    sc = SparkContext(conf=conf)
     
-    sqlContext = SQLContext(sc).read.format("jdbc") \
-                                    .options("url","")         
-    print("OK...")
+  
 
-
-
-
-
-
-
-
-class CassandraConnection():
-    cc_user_test='test'
-    cc_pwd_test='testquart'
    
-
 
 if __name__=='__main__':
     main()
+
+
+
+
+
+"""
+class CassandraConnection():
+    cc_user_test='test'
+    cc_pwd_test='testquart'
+"""
+   
+
+
+
 
