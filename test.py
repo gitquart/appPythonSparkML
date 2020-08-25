c5b2e1a28292a0ece024462d8eb6433a27f3b766
Ulyses Rico <ulyses@quart.com.mx>
2020-08-25 16:08:02 -0500

    First steps in pyspark...

--- test.py
+++ test.py
@@ -1,23 +1,22 @@
 import os
-from pyspark import SparkConf, SparkContext
-from pyspark.sql import SQLContext
+from pyspark import SparkContext,SparkFiles,SQLContext
+from pyspark.sql import SQLContext, SparkSession
 pathToHere=os.getcwd()
-cmd='--packages com.datastax.spark:spark-cassandra-connector_2.11:2.5.1 --files '+os.getcwd()+'\\secure-connect-dbtest.zip pyspark'
 
 #Complete command
 #pyspark --packages com.datastax.spark:spark-cassandra-connector_2.11:2.5.1 --files C:\Users\UlysesRico\Documents\quart\appPythonSparkML\secure-connect-dbtest.zip
 
-querySt_1M="select * from test.tbthesis where period_number>4 ALLOW FILTERING "
+#querySt_1M="select * from test.tbthesis where period_number>4 ALLOW FILTERING "
+#os.environ['JAVA_HOME']='C:\\Program Files\\Java\\jdk1.8.0_261'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--files '+os.getcwd()+'\\secure-connect-dbtest.zip pyspark'
 
 def main():
-    os.environ['PYSPARK_SUBMIT_ARGS'] = cmd
-    sc = SparkContext("local","appTest")
-    
-
-    
-
-    
    
+    session = SparkSession.builder.appName('SparkCassandraApp').config('spark.cassandra.connection.config.cloud.path',os.getcwd()+'\\secure-connect-dbtest.zip').config('spark.cassandra.auth.username','test').config('spark.cassandra.auth.password','testquart').getOrCreate()
+    sqlContext=SQLContext(session)
+    
+    
+
 
 if __name__=='__main__':
     main()
@@ -25,12 +24,6 @@ if __name__=='__main__':
 
 
 
-
-"""
-class CassandraConnection():
-    cc_user_test='test'
-    cc_pwd_test='testquart'
-"""
    
 
 
