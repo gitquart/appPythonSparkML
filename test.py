602bdb82319be0836052075185f3973d38802824
Ulyses Rico <ulyses@quart.com.mx>
2020-08-25 16:12:08 -0500

    ...

--- test.py
+++ test.py
@@ -5,15 +5,16 @@ pathToHere=os.getcwd()
 
 #Complete command
 #pyspark --packages com.datastax.spark:spark-cassandra-connector_2.11:2.5.1 --files C:\Users\UlysesRico\Documents\quart\appPythonSparkML\secure-connect-dbtest.zip
-
 #querySt_1M="select * from test.tbthesis where period_number>4 ALLOW FILTERING "
-#os.environ['JAVA_HOME']='C:\\Program Files\\Java\\jdk1.8.0_261'
-#os.environ['PYSPARK_SUBMIT_ARGS'] = '--files '+os.getcwd()+'\\secure-connect-dbtest.zip pyspark'
 
 def main():
    
     session = SparkSession.builder.appName('SparkCassandraApp').config('spark.cassandra.connection.config.cloud.path',os.getcwd()+'\\secure-connect-dbtest.zip').config('spark.cassandra.auth.username','test').config('spark.cassandra.auth.password','testquart').getOrCreate()
     sqlContext=SQLContext(session)
+    if sqlContext:
+        print('Good')
+
+    session.stop()
     
     
 
