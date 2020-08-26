18953200a87a00bade3f5b5d77093685d42d4b4e
Ulyses Rico <ulyses@quart.com.mx>
2020-08-26 12:20:09 -0500

    ...

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import os
-from pyspark import SparkContext,SparkFiles,SQLContext
+from pyspark import SparkContext,SparkFiles,SQLContext,SparkFiles
 from pyspark.sql import SQLContext, SparkSession
-import pyspark_cassandra
-pathToHere=os.getcwd()
+from pyspark.sql.functions import col
+
+
 
 #Complete command
 #pyspark  --files C:\Users\UlysesRico\Documents\quart\appPythonSparkML\secure-connect-dbtest.zip
@@ -10,7 +11,12 @@ pathToHere=os.getcwd()
 
 def main():
    
-   print("hey")
+    
+    secure_bundle_file=os.getcwd()+'\\secure-connect-dbtest.zip'
+    sparkSession = SparkSession.builder.appName('SparkCassandraApp').config('spark.cassandra.connection.config.cloud.path',secure_bundle_file).config('spark.cassandra.auth.username', 'test').config('spark.cassandra.auth.password','testquart').config('spark.dse.continuousPagingEnabled',False).master('local[*]').getOrCreate()
+    #Until here is fine, the "reading" is failing
+    data = sparkSession.read.format("org.apache.spark.sql.cassandra").options(table="tbthesis", keyspace="test").load()
+    data.count()
     
     
 
