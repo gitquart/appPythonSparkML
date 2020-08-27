8ce0dfc48a4898fd4ba6de72aac15cc869ea600c
Ulyses Rico <ulyses@quart.com.mx>
2020-08-27 14:40:46 -0500

    funciona

--- test.py
+++ test.py
@@ -3,25 +3,20 @@ from pyspark import SparkContext,SparkFiles,SQLContext,SparkFiles
 from pyspark.sql import SQLContext, SparkSession
 from pyspark.sql.functions import col
 
-
 #This package was downloaded without errors in C:\Users\UlysesRico\.ivy2\jars
 #--packages com.datastax.spark:spark-cassandra-connector_2.11:2.0.0-M3
 #querySt_1M="select * from test.tbthesis where period_number>4 ALLOW FILTERING "
 #pyspark shell is pyspark command
 
-
-
 def main():
     
-    #os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.11:2.0.0-M3  pyspark'
+    #os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.11:2.3.0 pyspark'
     secure_bundle_file=os.getcwd()+'\\secure-connect-dbtest.zip'
-    sparkSession = SparkSession.builder.appName('SparkCassandraApp').config('spark.cassandra.connection.config.cloud.path',secure_bundle_file).config('spark.cassandra.auth.username', 'test').config('spark.cassandra.auth.password','testquart').config('spark.dse.continuousPagingEnabled',False).config('spark.cassandra.connection.host','c58d5d9e-015a-4255-8f6c-05784a7c59ba-us-east1.db.astra.datastax.com').master('local[2]').getOrCreate()
+    sparkSession = SparkSession.builder.appName('SparkCassandraApp').config('spark.cassandra.connection.config.cloud.path',secure_bundle_file).config('spark.cassandra.auth.username', 'test').config('spark.cassandra.auth.password','testquart').config('spark.dse.continuousPagingEnabled',False).config('spark.cassandra.connection.host','c58d5d9e-015a-4255-8f6c-05784a7c59ba-us-east1.db.astra.datastax.com').config("spark.jars.packages", "com.datastax.spark:spark-cassandra-connector_2.11:2.5.1").master('local[2]').getOrCreate()
     #Until here is fine, the "reading" is failing
     data = sparkSession.read.format("org.apache.spark.sql.cassandra").options(table="tbthesis", keyspace="test").load()
     data.count()
     
-    
-
 
 if __name__=='__main__':
     main()
