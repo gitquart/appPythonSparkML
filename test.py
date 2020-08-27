f5e260aea5b4083ed28f9f150698cc07d7418c3a
Ulyses Rico <ulyses@quart.com.mx>
2020-08-27 10:48:14 -0500

    ...

--- test.py
+++ test.py
@@ -4,16 +4,18 @@ from pyspark.sql import SQLContext, SparkSession
 from pyspark.sql.functions import col
 
 
-
-#Complete command
-#pyspark  --files C:\Users\UlysesRico\Documents\quart\appPythonSparkML\secure-connect-dbtest.zip
+#This package was downloaded without errors in C:\Users\UlysesRico\.ivy2\jars
+#--packages com.datastax.spark:spark-cassandra-connector_2.11:2.0.0-M3
 #querySt_1M="select * from test.tbthesis where period_number>4 ALLOW FILTERING "
+#pyspark shell is pyspark command
+
+
 
 def main():
-   
     
+    #os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.11:2.0.0-M3  pyspark'
     secure_bundle_file=os.getcwd()+'\\secure-connect-dbtest.zip'
-    sparkSession = SparkSession.builder.appName('SparkCassandraApp').config('spark.cassandra.connection.config.cloud.path',secure_bundle_file).config('spark.cassandra.auth.username', 'test').config('spark.cassandra.auth.password','testquart').config('spark.dse.continuousPagingEnabled',False).master('local[*]').getOrCreate()
+    sparkSession = SparkSession.builder.appName('SparkCassandraApp').config('spark.cassandra.connection.config.cloud.path',secure_bundle_file).config('spark.cassandra.auth.username', 'test').config('spark.cassandra.auth.password','testquart').config('spark.dse.continuousPagingEnabled',False).config('spark.cassandra.connection.host','c58d5d9e-015a-4255-8f6c-05784a7c59ba-us-east1.db.astra.datastax.com').master('local[2]').getOrCreate()
     #Until here is fine, the "reading" is failing
     data = sparkSession.read.format("org.apache.spark.sql.cassandra").options(table="tbthesis", keyspace="test").load()
     data.count()
