028f0e98d4faa442247152c9e1f4275e7b5ce3d6
Ulyses Rico <ulyses@quart.com.mx>
2020-08-28 09:21:08 -0500

    Finally, download of cassandra connectors done!

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ from pyspark.sql.functions import col
 
 #querySt_1M="select * from test.tbthesis where period_number>4 ALLOW FILTERING "
 #pyspark shell is pyspark command
+#If the jars are not downloaded yet, use this line:
+#sparkSession = SparkSession.builder.appName('SparkCassandraApp').config('spark.cassandra.connection.config.cloud.path',secure_bundle_file).config('spark.cassandra.auth.username', 'test').config('spark.cassandra.auth.password','testquart').config('spark.dse.continuousPagingEnabled',False).config('spark.cassandra.connection.host','c58d5d9e-015a-4255-8f6c-05784a7c59ba-us-east1.db.astra.datastax.com').config("com.datastax.spark:spark-cassandra-connector_2.11:2.5.1").master('local[*]').getOrCreate()
 
 def main():
     
@@ -12,7 +14,7 @@ def main():
     #os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.12:2.5.0 --master spark://192.168.1.66:4040/ pyspark'
     
     secure_bundle_file=os.getcwd()+'\\secure-connect-dbtest.zip'
-    sparkSession = SparkSession.builder.appName('SparkCassandraApp').config('spark.cassandra.connection.config.cloud.path',secure_bundle_file).config('spark.cassandra.auth.username', 'test').config('spark.cassandra.auth.password','testquart').config('spark.dse.continuousPagingEnabled',False).config('spark.cassandra.connection.host','c58d5d9e-015a-4255-8f6c-05784a7c59ba-us-east1.db.astra.datastax.com').config("com.datastax.spark:spark-cassandra-connector_2.11:2.5.1").master('local[*]').getOrCreate()
+    sparkSession = SparkSession.builder.appName('SparkCassandraApp').config('spark.cassandra.connection.config.cloud.path',secure_bundle_file).config('spark.cassandra.auth.username', 'test').config('spark.cassandra.auth.password','testquart').config('spark.dse.continuousPagingEnabled',False).config('spark.cassandra.connection.host','c58d5d9e-015a-4255-8f6c-05784a7c59ba-us-east1.db.astra.datastax.com').master('local[*]').getOrCreate()
     #Until here is fine, the "reading" is failing
     data = sparkSession.read.format("org.apache.spark.sql.cassandra").options(table="tbthesis", keyspace="test").load()
     data.count()
