9955c1323bb1ce5f10e53555f824724fdc3035dd
Ulyses Rico <ulyses@quart.com.mx>
2020-08-28 13:03:24 -0500

    ...

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ def main():
    
     sparksn = SparkSession.builder.appName('SparkCassandraApp').config('spark.cassandra.connection.config.cloud.path',secure_bundle_file).config('spark.cassandra.auth.username', 'test').config('spark.cassandra.auth.password','testquart').config('spark.dse.continuousPagingEnabled',False).master('local[*]').getOrCreate()
     sqlCxt= SQLContext(sparksn)
-    df=sqlCxt.read.format("org.apache.spark.sql.cassandra").options(table='tbthesis',keyspace='test').load().take(2)
+    df=sqlCxt.read.format("org.apache.spark.sql.cassandra").options(table='tbthesis',keyspace='test').load().show()
     #dataframe = sparkSession.read.format("org.apache.spark.sql.cassandra").options(table='tbthesis',keyspace='test').load()
     #dataframe.count()
     print('ok')
