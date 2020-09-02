5ce9f2a5a005884f04d732dbc780ddcaf0089966
Ulyses Rico <ulyses@quart.com.mx>
2020-09-02 11:14:06 -0500

    ...

--- test.py
+++ test.py
@@ -9,12 +9,10 @@ secure_bundle_file=os.getcwd()+'\\secure-connect-dbtest.zip'
 def main():
       
    
-    sparksn = SparkSession.builder.appName('SparkCassandraApp').config('spark.cassandra.connection.config.cloud.path',secure_bundle_file).config('spark.cassandra.auth.username', 'test').config('spark.cassandra.auth.password','testquart').config('spark.dse.continuousPagingEnabled',False).master('local[*]').getOrCreate()
-    #sqlCxt= SQLContext(sparksn)
-    #The problem is coming at load()
+    sparksn = SparkSession.builder.appName('SparkCassandraApp').config('spark.cassandra.connection.config.cloud.path',secure_bundle_file).config('spark.cassandra.auth.username', 'test').config('spark.cassandra.auth.password','testquart').config('spark.dse.continuousPagingEnabled',False).getOrCreate()
     df=sparksn.read.format("org.apache.spark.sql.cassandra").options(table='tbthesis',keyspace='test').load()
     df.count()
-    print('ok')
+   
     
 
 if __name__=='__main__':
